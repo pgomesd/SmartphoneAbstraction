6dea14ed1f9ec8e8be5e0f437cb9be79dc33b69f
pgomesd <pablogomes.cel@gmail.com>
2022-10-28 15:29:07 -0300

    Desafio concluido.

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,21 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        // Implementado.
+        private string Modelo { get; set; }
 
-        public Smartphone(string numero)
+        private string IMEI { get; set; }
+
+        private int Memoria { get; set; }
+
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+// Testes realizados.
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone("123456789", "XR", "00000000", 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("BeReal");
+iphone.ReceberLigacao();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia("987654321", "5.4", "99999999", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Snake");
+nokia.ReceberLigacao();
